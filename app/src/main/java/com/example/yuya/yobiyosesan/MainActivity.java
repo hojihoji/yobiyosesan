6f0083e77645d06c767d17677c3a0c5539510aae
yuya <yuya.m.tokai@gmail.com>
2018-09-19 20:08:47 +0900

    2nd commit

--- app/src/main/java/com/example/yuya/yobiyosesan/MainActivity.java
+++ app/src/main/java/com/example/yuya/yobiyosesan/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.yuya.yobiyosesan;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    MediaPlayer mp = null;
+    Button play_button;
+    Button stop_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mp = MediaPlayer.create(this, R.raw.sample);
+
+        play_button = (Button) findViewById(R.id.btnPlay);
+        play_button.setOnClickListener(this);
     }
+
+    public void onClick(View v){
+
+    }
+
 }
